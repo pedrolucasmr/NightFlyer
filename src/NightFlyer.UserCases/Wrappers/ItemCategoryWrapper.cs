3af7d054b7a5d71c75ed30689c607ce5966c6c10
Pedro <pl.maia@hotmail.com>
2024-10-02 18:04:18 -0300

    start building the item creation flow

--- src/NightFlyer.UserCases/Wrappers/ItemCategoryWrapper.cs
+++ src/NightFlyer.UserCases/Wrappers/ItemCategoryWrapper.cs
@@ -1,11 +1,6 @@
 ﻿using NightFlyer.UseCases.DataModels;
 using NightFlyer.UseCases.RepositoryContracts;
 using NightFlyer.UseCases.Wrappers.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NightFlyer.UseCases.Wrappers
 {
